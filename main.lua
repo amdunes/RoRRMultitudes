585e16880571dc654ac42004bd3b5cda7807ac19
amdunes <161895636+amdunes@users.noreply.github.com>
2024-04-18 18:20:10 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,10 @@
+local function --the function()
+    -- Replace "increase_player_count_function" with the actual function name
+    increase_player_count_function()
+end
+
+gui.add_always_draw_imgui(function()
+    if ImGui.IsKeyDown(ImGuiKeyMod.Shift) and ImGui.IsKeyPressed(ImGuiKey.A) then
+        --the function if i find it()
+    end
+end)
